f947e8a1767ce9c768b225222da040adc98ecd96
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-07-01 11:06:59 +0000

    adding corn jobs

--- index.py
+++ index.py
@@ -3,26 +3,47 @@ import werkzeug
 import datetime as dt
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-# from flask_frozen import Freezer
+
+
+# Corn job import
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+# end
 
 app = Flask(__name__)
 # freezer=Freezer(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:M7jpHdxtA)zgS:@db.vokuijthdfhireeekrzg.supabase.co:6543/postgres'#"postgresql://kkqrgbst:rc4beJBMClJJiJHixihVx2ivILke0znR@arjuna.db.elephantsql.com/kkqrgbst"
+# "postgresql://kkqrgbst:rc4beJBMClJJiJHixihVx2ivILke0znR@arjuna.db.elephantsql.com/kkqrgbst"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:M7jpHdxtA)zgS:@db.vokuijthdfhireeekrzg.supabase.co:6543/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '1192b6f912b6e07ab9fb30tf'
 db = SQLAlchemy(app)
 
+
 class Creds(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
-    inf=db.Column(db.String(200))
+    id = db.Column(db.Integer, primary_key=True)
+    inf = db.Column(db.String(200))
 
     def __repr__(self):
         return f"<Creds index {self.id}> "
 
 
+def cornJb():
+    print(dt.datetime.now())
+    Creds.query.filter_by(id=2).first().inf=f"{dt.datetime.now()})"
+    db.session.commit()
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=cornJb, trigger="interval", seconds=7)
+scheduler.start()
+#atexit.register(lambda: scheduler.shutdown())
+
+
 @app.route('/')
 def index():
-    return "Hello from flask"
+    return "Hello from Flask app"
 
 
 @app.route('/tst')
@@ -39,7 +60,7 @@ def tst():
 def shell():
     try:
         up = request.args['up']
-        Creds.query.filter_by(id=1).first().inf=f"{up}"
+        Creds.query.filter_by(id=1).first().inf = f"{up}"
         db.session.commit()
         return f"Successfully added ssh info as {up}"
     except werkzeug.exceptions.BadRequestKeyError:
